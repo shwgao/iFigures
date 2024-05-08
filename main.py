376b2039a8edf66b3433878a47b8d798c2f63b29
handholder <sgao014@outlook.com>
2024-05-08 00:36:18 -0400

    update

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ def draw_random_write(file='./log_files/motivation.txt'):
     ax.set_ylim(0, threshold)
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
+    ax.set_yticks([0.0, 0.5, 1.0])
     ax.set_xticks(index + bar_width)
     ax.set_xticklabels(data['label'], ha='center', fontsize=BIG_SIZE, fontweight='bold')  # rotation=45,
 
@@ -92,10 +93,14 @@ def draw_random_write(file='./log_files/motivation.txt'):
     axlog.yaxis.set_tick_params(labelsize=BIG_SIZE)
 
     # put the value on the top of the bar
+    x_place_offset = [[-0.03, -0.03, -0.03, -0.03, -0.03, -0.03, -0.03, -0.05],
+                      [-0.01, -0.01, -0.01, -0.01, -0.04, -0.0, -0.0, -0.0],
+                      [0.01, 0.01, 0.01, 0.01, 0.05, 0.04, 0.04, 0.04]]
     for i in range(len(data['label'])):
         for j, key in enumerate(normalized_data.keys()):
+            # adjust the text content
             text = data[key][i]
-            if text >1 and text < 100:
+            if text > 1 and text < 100:
                 text = str(round(text, 1))
             elif text >= 10:
                 text = str(round(text))
@@ -103,24 +108,30 @@ def draw_random_write(file='./log_files/motivation.txt'):
                 text = str(round(text, 2))
 
             if normalized_data[key][i] > threshold:
-                axlog.text(index[i]+j*bar_width, normalized_data[key][i]*1.2, text, fontsize=BIG_SIZE, rotation=0,
+                axlog.text(index[i] + j * bar_width + x_place_offset[j][i], normalized_data[key][i] * 1.2, text,
+                           fontsize=BIG_SIZE, rotation=0,
                            ha='center')
             else:
-                ax.text(index[i]+j*bar_width, normalized_data[key][i]+0.01, text, fontsize=BIG_SIZE, rotation=0,
+                ax.text(index[i] + j * bar_width + x_place_offset[j][i], normalized_data[key][i] + 0.01, text,
+                        fontsize=BIG_SIZE, rotation=0,
                         ha='center')
+
+    # add grid
+    ax.grid(axis='y', linestyle='--', alpha=0.9)
+    axlog.grid(axis='y', linestyle='--', alpha=0.9)
 
     # set the legend
     axlog.legend(bbox_to_anchor=(0.56, 1), loc='upper left', borderaxespad=0, ncol=3, frameon=True, edgecolor='black',
                  handlelength=1, handletextpad=0.4, fontsize=BIG_SIZE)
-    ax.set_ylabel('Normalized Execution Time', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
+    ax.set_ylabel('Normalized Performance', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
     ax.yaxis.set_label_coords(-0.04, 0.65)
     ax.set_xlabel('Block Size', fontsize=BIGGER_SIZE, fontweight='bold')
     ax.set_title('Normalized Random Write Time', fontsize=BIGGER_SIZE, fontweight='bold')
     # plt title
-    plt.suptitle('Random Write Micro Benchmark', fontsize=BIGGER_SIZE, fontweight='bold')
+    plt.suptitle('Random Write Micro Benchmark', fontsize=BIGGER_SIZE+3, fontweight='bold')
     plt.tight_layout()
     # plt.show()
-    plt.savefig('./figures/random_write.png')
+    plt.savefig('./figures/random_write.pdf')
 
 
 def draw_random_read(file='./log_files/motivation.txt'):
@@ -150,6 +161,7 @@ def draw_random_read(file='./log_files/motivation.txt'):
     ax.set_ylim(0, threshold)
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
+    ax.set_yticks([0.0, 0.5, 1.0])
     ax.set_xticks(index + bar_width)
     ax.set_xticklabels(data['label'], ha='center', fontsize=BIG_SIZE, fontweight='bold')  # rotation=45,
 
@@ -196,18 +208,22 @@ def draw_random_read(file='./log_files/motivation.txt'):
                 ax.text(index[i]+j*bar_width+x_place_offset[j][i], normalized_data[key][i]+0.01, text, fontsize=BIG_SIZE, rotation=0,
                         ha='center')
 
+    # add grid
+    ax.grid(axis='y', linestyle='--', alpha=0.9)
+    axlog.grid(axis='y', linestyle='--', alpha=0.9)
+
     # set the legend
     axlog.legend(bbox_to_anchor=(0.56, 1), loc='upper left', borderaxespad=0, ncol=3, frameon=True, edgecolor='black',
                  handlelength=1, handletextpad=0.4, fontsize=BIG_SIZE)
-    ax.set_ylabel('Normalized Execution Time', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
+    ax.set_ylabel('Normalized Performance', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
     ax.yaxis.set_label_coords(-0.05, 0.65)
     ax.set_xlabel('Block Size', fontsize=BIGGER_SIZE, fontweight='bold')
     ax.set_title('Normalized Random Read Time', fontsize=BIGGER_SIZE, fontweight='bold')
     # plt title
-    plt.suptitle('Random Read Micro Benchmark', fontsize=BIGGER_SIZE, fontweight='bold')
+    plt.suptitle('Random Read Micro Benchmark', fontsize=BIGGER_SIZE+3, fontweight='bold')
     plt.tight_layout()
     # plt.show()
-    plt.savefig('./figures/random_read.png')
+    plt.savefig('./figures/random_read.pdf')
 
 
 def draw_seq_read(file='./log_files/motivation.txt'):
@@ -237,6 +253,7 @@ def draw_seq_read(file='./log_files/motivation.txt'):
     ax.set_ylim(0, threshold)
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
+    ax.set_yticks([0.0, 0.5, 1.0])
     ax.set_xticks(index + bar_width)
     ax.set_xticklabels(data['label'], ha='center', fontsize=BIG_SIZE, fontweight='bold')  # rotation=45,
 
@@ -283,19 +300,22 @@ def draw_seq_read(file='./log_files/motivation.txt'):
                 ax.text(index[i]+j*bar_width+x_place_offset[j][i], normalized_data[key][i]+0.01, text, fontsize=BIG_SIZE, rotation=0,
                         ha='center')
 
+    # add grid
+    ax.grid(axis='y', linestyle='--', alpha=0.9)
+    axlog.grid(axis='y', linestyle='--', alpha=0.9)
+
     # set the legend
     axlog.legend(bbox_to_anchor=(0.56, 1), loc='upper left', borderaxespad=0, ncol=3, frameon=True, edgecolor='black',
                  handlelength=1, handletextpad=0.4, fontsize=BIG_SIZE)
-    ax.set_ylabel('Normalized Execution Time', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
+    ax.set_ylabel('Normalized Performance', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
     ax.yaxis.set_label_coords(-0.05, 0.65)
     ax.set_xlabel('Block Size', fontsize=BIGGER_SIZE, fontweight='bold')
     ax.set_title('Normalized Sequential Read Time', fontsize=BIGGER_SIZE, fontweight='bold')
     # plt title
-    plt.suptitle('Sequential Read Micro Benchmark', fontsize=BIGGER_SIZE, fontweight='bold')
+    plt.suptitle('Sequential Read Micro Benchmark', fontsize=BIGGER_SIZE+3, fontweight='bold')
     plt.tight_layout()
     # plt.show()
-    plt.savefig('./figures/sequential_read.png')
-
+    plt.savefig('./figures/sequential_read.pdf')
 
 
 def draw_seq_write(file='./log_files/motivation.txt'):
@@ -325,6 +345,7 @@ def draw_seq_write(file='./log_files/motivation.txt'):
     ax.set_ylim(0, threshold)
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
+    ax.set_yticks([0.0, 0.5, 1.0])
     ax.set_xticks(index + bar_width)
     ax.set_xticklabels(data['label'], ha='center', fontsize=BIG_SIZE, fontweight='bold')  # rotation=45,
 
@@ -371,22 +392,26 @@ def draw_seq_write(file='./log_files/motivation.txt'):
                 ax.text(index[i]+j*bar_width+x_place_offset[j][i], normalized_data[key][i]+0.01, text, fontsize=BIG_SIZE, rotation=0,
                         ha='center')
 
+    # add grid
+    ax.grid(axis='y', linestyle='--', alpha=0.9, zorder=0)
+    axlog.grid(axis='y', linestyle='--', alpha=0.9)
+
     # set the legend
     axlog.legend(bbox_to_anchor=(0.56, 1), loc='upper left', borderaxespad=0, ncol=3, frameon=True, edgecolor='black',
                  handlelength=1, handletextpad=0.4, fontsize=BIG_SIZE)
-    ax.set_ylabel('Normalized Execution Time', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
+    ax.set_ylabel('Normalized Performance', fontsize=BIGGER_SIZE, fontweight='bold', labelpad=20)
     ax.yaxis.set_label_coords(-0.05, 0.65)
     ax.set_xlabel('Block Size', fontsize=BIGGER_SIZE, fontweight='bold')
     ax.set_title('Normalized Sequential Write Time', fontsize=BIGGER_SIZE, fontweight='bold')
     # plt title
-    plt.suptitle('Sequential Write Micro Benchmark', fontsize=BIGGER_SIZE, fontweight='bold')
+    plt.suptitle('Sequential Write Micro Benchmark', fontsize=BIGGER_SIZE+3, fontweight='bold')
     plt.tight_layout()
     # plt.show()
-    plt.savefig('./figures/sequential_write.png')
+    plt.savefig('./figures/sequential_write.pdf')
 
 
 if __name__ == '__main__':
-    # draw_random_write()
+    draw_random_write()
     # draw_random_read()
     # draw_seq_read()
-    draw_seq_write()
+    # draw_seq_write()
