e55029a81e904f47ac1d1b1761603d3d3b8d4895
handholder <sgao014@outlook.com>
2024-05-08 22:50:24 -0400

    update

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def read_data(file='./log_files/motivation.txt'):
     data = {}
     with open(file, 'r') as f:
         for head_line in f:
-            if 'write' in head_line or 'read' in head_line:
+            if len(head_line.split()) <= 2:
                 sub_data = {}
                 for line in f:
                     if line[0] == '\n':
@@ -410,8 +410,164 @@ def draw_seq_write(file='./log_files/motivation.txt'):
     plt.savefig('./figures/sequential_write.pdf')
 
 
+def draw_cg_perform():
+    datas = read_data('./log_files/motivation.txt')
+    data = datas['CG performance']
+
+    line_data = [x/(1024**2) for x in data['PeakMem']]
+    bar_data = [x/60 for x in data['Exetime']]
+
+    x_ticklabels = data['label']
+    title = 'CG'
+
+    line_label = 'Peak Memory Usage (GB)'
+    bar_label = 'Execution Time (min)'
+    xlabel = ''
+
+    bar_scale = 'linear'
+    line_scale = 'linear'
+
+    bar_color = colors[0]
+    line_color = colors[1]
+
+    file_name = './figures/CG_performance.pdf'
+
+    y_tickles = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220]
+
+    draw_double_axis(bar_data, line_data, bar_label, line_label, title, xlabel, x_ticklabels, file_name, bar_color,
+                     line_color, bar_scale, line_scale, fig_size=(13, 8), font_size=BIGGER_SIZE, y_tickles=y_tickles)
+
+
+def draw_double_axis(bar_data, line_data, bar_label, line_label, title, xlabel, x_ticklabels, file_name, bar_color,
+                     line_color, bar_scale='linear', line_scale='linear', fig_size=(20, 8), font_size=BIGGER_SIZE,
+                     y_tickles=None):
+    fig, ax = plt.subplots(1, 1, figsize=fig_size)
+    ax2 = ax.twinx()
+
+    index = np.arange(len(bar_data))*0.5
+
+    # 绘制柱状图
+    bar_width = 0.3
+    ax.bar(index, bar_data, bar_width, color=bar_color, edgecolor='black')
+    ax.set_ylabel(bar_label, fontsize=font_size, fontweight='bold')
+    ax.set_xlabel(xlabel, fontsize=font_size, fontweight='bold')
+    ax.set_title(title, fontsize=font_size+3, fontweight='bold')
+
+    # 绘制折线图
+    ax2.plot(index, line_data, color=line_color, marker='o', markersize=15, label=line_label, linewidth=4)
+    ax2.set_ylabel(line_label, fontsize=font_size, fontweight='bold')
+
+    # 调整x轴刻度标签
+    ax.set_xticks(index)
+    ax.set_xticklabels(x_ticklabels, fontsize=font_size, fontweight='bold')
+
+    # set y ticks' format
+    ax.yaxis.set_tick_params(labelsize=BIG_SIZE)
+    ax2.yaxis.set_tick_params(labelsize=BIG_SIZE)
+
+    # set scale
+    ax.set_yscale(bar_scale)
+    ax2.set_yscale(line_scale)
+
+    # 调整y轴范围
+    ax.set_ylim(0, 225)
+    ax2.set_ylim(0, 9.3)
+    if y_tickles:
+        ax.set_yticks()
+    # ax.autoscale()
+    # ax2.autoscale()
+
+    # put the number near the line
+    for i in range(len(line_data)):
+        ax2.text(index[i], line_data[i], round(line_data[i], 1), ha='center', va='bottom', fontsize=BIG_SIZE)
+
+    bar_proxy = plt.Rectangle((0, 0), 1, 1, fc=bar_color)
+    line_proxy = plt.Line2D([0], [0], color=line_color, lw=2)
+    ax.legend(handles=[bar_proxy, line_proxy], labels=[bar_label, line_label], loc='upper right', fontsize=BIG_SIZE,
+              edgecolor='black', frameon=True, handlelength=1, handletextpad=0.4, bbox_to_anchor=(0.8, 1.02))
+
+    ax.grid(axis='y', linestyle='--', alpha=0.9, zorder=0)
+
+    plt.tight_layout()
+
+    # 保存图片
+    plt.savefig(file_name)
+    # plt.close()
+    plt.show()
+
+
+def draw_NP_1():
+    labels = ['Init', '1', '2', '3', '4', '5']
+    data1 = [142014098, 0, 0, 0, 0, 258]
+    data2 = [108, 2, 0, 0, 1, 109]
+    data3 = [93.53, 84.37, 84.37, 84.37, 84.37, 88.56, 95.23, 0.27]
+    data = [data1, data2, data3]
+    file_names = ['./figures/NP_1_1.pdf', './figures/NP_1_2.pdf', './figures/NP_1_3.pdf']
+    colors = ['#4793AF', '#4793AF', '#FFC470']
+
+    # draw three bars separately
+    for i in range(3):
+        fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+        labels = ['Init', '1', '2', '3', '4', '5', 'Do>4KB', 'Do<4KB'] if i == 2 else labels
+
+        ax.bar(labels, data[i], color=colors[i], edgecolor='black')
+
+        ax.set_ylabel('Number of Data Object', fontsize=BIGGER_SIZE, fontweight='bold')
+        ax.set_xticklabels(labels, fontsize=BIGGER_SIZE, fontweight='bold')
+        ax.yaxis.set_tick_params(labelsize=BIGGER_SIZE)
+        ax.set_xlabel('Iteration', fontsize=BIGGER_SIZE, fontweight='bold')
+
+        if i == 0:
+            ax.set_title('Data Object Size <= 4KB', fontsize=BIGGER_SIZE + 3, fontweight='bold')
+            ax.autoscale()
+            ax.set_yscale('log')
+            ax.set_ylim(0, 4e8)
+            plt.subplots_adjust(left=0.15)
+            place = [j if j > 200 else 150 for j in data[i]]
+            text = ['1.4e+08', '0', '0', '0', '0', '258']
+        elif i == 1:
+            ax.set_title('Data Object Size >= 4KB', fontsize=BIGGER_SIZE + 3, fontweight='bold')
+            ax.set_yscale('log')
+            ax.set_ylim(0, 2e2)
+
+            plt.subplots_adjust(left=0.15)  # 增加left的值
+            place = [j if j >= 1 else 0.8 for j in data[i]]
+            text = data[i]
+        else:
+            ax.set_title('Peak Memory Usage', fontsize=BIGGER_SIZE, fontweight='bold')
+            ax.set_ylabel('Peak Memory Usage(GB)', fontsize=BIGGER_SIZE, fontweight='bold')
+            # set ax[0]'s y-axis scientific notation
+            # ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+            # ax.tick_params(axis='y', labelsize=BIGGER_SIZE)
+            text = [str(round(j)) for j in data[i]]
+            text[-1] = '27MB'
+            ax.set_ylim(0, 110)
+            place = data[i]
+
+            labels = ax.get_xticklabels()
+            labels[-2].set_fontsize(BIGGER_SIZE-8)
+            labels[-1].set_fontsize(BIGGER_SIZE-8)
+            labels[-2].set_rotation(15)
+            labels[-1].set_rotation(15)
+            ax.set_xticklabels(labels)
+
+        # put the number on the top of the bar
+        for j in range(len(labels)):
+            # if number greater than 1 Million, use scientific notation
+            ax.text(j, place[j], text[j], ha='center', va='bottom', fontsize=BIG_SIZE)
+
+        ax.grid(axis='y', linestyle='--', alpha=0.9, zorder=0)
+
+        plt.tight_layout()
+
+        plt.savefig(file_names[i])
+        plt.show()
+
+
 if __name__ == '__main__':
-    draw_random_write()
+    # draw_random_write()
     # draw_random_read()
     # draw_seq_read()
     # draw_seq_write()
+    # draw_cg_perform()
+    draw_NP_1()
